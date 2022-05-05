521725e34acfcbda4f2e89f9d85a194b2a13213c
Sihao Yang <89471780+sihaoyan@users.noreply.github.com>
2022-05-04 17:46:30 -0700

    Add files via upload

---
+++ back-end/Util/Task.java
@@ -0,0 +1,23 @@
+package Util;
+
+import java.util.ArrayList;
+
+public class Task {
+	private String TaskName;
+	private String TaskDueDate;
+	
+	public void setTaskName(String name) {
+		TaskName = name;
+	}
+	
+	public void setDueDate(String date) {
+		TaskDueDate = date;
+	}
+	public String getTaskName() {
+		return TaskName;
+	}
+	
+	public String TaskDueDate() {
+		return TaskDueDate;
+	}
+}
