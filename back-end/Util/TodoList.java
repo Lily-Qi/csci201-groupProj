d94ddacc3c4d9ec500adb62d6f0c641b66937dee
Christopher Merino Brito <merinobr@usc.edu>
2022-05-05 19:55:34 -0700

    back-end stuff

---
+++ back-end/Util/TodoList.java
@@ -0,0 +1,15 @@
+package Util;
+
+import java.util.ArrayList;
+
+public class TodoList {
+	String TodolistId;
+	ArrayList<Task> TodoList = new ArrayList<Task>();
+	public void addTask(Task t) {
+		TodoList.add(t);
+	}
+	
+	public ArrayList<Task> getTodoList(){
+		return TodoList;
+	}
+}
